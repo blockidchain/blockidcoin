f2a91e0331167d29057686452834a08667d4f46b
mdfkbtc <mdfkbtc@veles.network>
2020-04-25 07:24:33 +0200

    Integrate HDchain functions cypter.cpp/.h

--- src/crypter.h
+++ src/crypter.h
@@ -123,7 +123,7 @@ class CCryptoKeyStore : public CBasicKeyStore
 {
 private:
     CKeyingMaterial vMasterKey;
-
+    CHDChain cryptedHDChain;
     //! if fUseCrypto is true, mapKeys must be empty
     //! if fUseCrypto is false, vMasterKey must be empty
     bool fUseCrypto;
@@ -137,6 +137,10 @@ protected:
     //! will encrypt previously unencrypted keys
     bool EncryptKeys(CKeyingMaterial& vMasterKeyIn);
 
+    bool EncryptHDChain(const CKeyingMaterial& vMasterKeyIn);
+    bool DecryptHDChain(CHDChain& hdChainRet) const;
+    bool SetHDChain(const CHDChain& chain);
+    bool SetCryptedHDChain(const CHDChain& chain);
     bool Unlock(const CKeyingMaterial& vMasterKeyIn);
 
     CryptedKeyMap mapCryptedKeys;
@@ -193,6 +197,8 @@ public:
         }
     }
 
+    bool GetHDChain(CHDChain& hdChainRet) const;
+    
     bool GetDeterministicSeed(const uint256& hashSeed, uint256& seed);
     bool AddDeterministicSeed(const uint256& seed);
 
